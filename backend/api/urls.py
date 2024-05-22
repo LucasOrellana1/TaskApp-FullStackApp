f9b4f17dc269607bbf1429875f73479c7041e9d4
LucasOrellana1 <orellanalu140@gmail.com>
2024-05-22 17:52:45 -0400

    2 commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+#
 urlpatterns = [
     #as_view transforma una vista de clase a una funcional
     path("notes/", views.CreateNoteView.as_view(), name = "note-list"),
